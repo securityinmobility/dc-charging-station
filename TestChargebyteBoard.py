fd1330acdef53e71e70e8944a016da1958a48989
gisela moreira ortt <ms.giselaortt@gmail.com>
2024-05-13 18:49:50 +0200

    coded some more tests, now many bugs were undercovered

--- TestChargebyteBoard.py
+++ TestChargebyteBoard.py
@@ -3,6 +3,8 @@ import socket
 import pytest
 import unittest.mock as mock
 import time
+from multipledispatch import dispatch
+from typing import overload
 
 
 @pytest.fixture(autouse=True)
@@ -20,10 +22,15 @@ def control(mocker, mock_socket):
 
     return control
 
+@dispatch(bytearray)
+def xor_calculator( parameters:bytearray ) -> int:
+    return 0
 
-def xor_calculator( parameters:list[int] ) -> int:
-    answer = parameters [0]
-    for number in parameters[1:]:
+
+@dispatch(list)
+def xor_calculator( parameter:list[int] ) -> int:
+    answer = parameter [0]
+    for number in parameter[1:]:
         answer = answer ^ number
     return answer
 
@@ -78,124 +85,153 @@ class TestChargeboardByte:
 
 
     def test_test_device_two(self, control, mock_socket):
-        data = bytearray([0x00,0x84,0x70,0x70,0x02,0x00])
+        data = bytearray([0x00,0x84,0x07,0x00,0x02,0x00])
         mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [0x02, 0x6, data ]
         build, reset_value = control.test_device_two()
-        assert build == 11312
+        assert build == 7
         assert reset_value.value == 0x02
         mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x04,0x05]))
 
 
     def test_set_pwm(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x91,0x00,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data]
+        control.set_pwm(2,3)
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x07,0x00,0x11,0x02,0x00,0x03,0x00,0x14]))
 
 
     def test_get_pwm(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x90,0x02,0x00,0x03,0x00,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x07, data ]
+        frequency, dutycicle = control.get_pwm()
+        assert frequency == 2
+        assert dutycicle == 3
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x10,0x11]))
 
 
     def test_control_pwm(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x92,0x01,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        control_code = control.control_pwm(1)
+        assert control_code.value == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x12,0x01,0x14]))
 
 
     def test_get_ucp(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x94,0x06,0x00,0x02,0x00,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x07, data ]
+        positive_cp, negative_cp = control.get_ucp()
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x14,0x015]))
+        assert positive_cp == 6
+        assert negative_cp == 2
 
 
     def test_set_ucp(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x95,0x01,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        resistance = control.set_ucp(1)
+        assert resistance == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x15,0x01,0x13]))
 
 
     def test_lock_and_unlock_cable_one(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x97,0x01,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.lock_unlock_cable_one(2)
+        assert answer.value == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x17,0x02,0x11]))
 
 
     def test_lock_and_unlock_cable_two(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x98,0x01,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.lock_unlock_cable_two(2)
+        assert answer.value == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x18,0x02,0x11]))
 
 
     def test_get_lock_motor_fault_pin(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0x9A,0x81,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.get_motor_fault_pin()
+        assert answer.value == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x1A,0x1b]))
 
 
     def test_set_cyclic_process_data(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0xA0,0x01,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.set_cyclic_process_data(4)
+        assert answer.value == 1
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x20,0x04,0x15]))
 
 
     def test_cyclic_process_data(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
-        mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
         pass
 
 
     def test_push_button_simple_connect(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0xB1,0x00,0x0])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.push_button_simple_connect(1)
+        assert answer.value == 0
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x31,0x01,0x15]))
 
 
-    def test_execute_software_reset_on_device(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+    def test_reset(self,control,mock_socket):
+        data = bytearray([0x00,0xB3,0x00,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
-
-
-    def test_x_is_sent_by_device_after_reset(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
-        mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        control.reset()
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x33,0x15]))
 
 
     def test_activate_proximity_pilot_resistor_(self,control,mock_socket):
-        data = bytearray([0x00,0x81,0x81,0x81,0x02,0x00])
+        data = bytearray([0x00,0xD0,0x00,0x00])
         mock_socket.recv = mock.Mock()
-        mock_socket.recv.side_effect = [0x02, 0x06, data ]
-        control.test_device_one()
-        pass
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.activate_proximity_pilot_resistor(0x06)
+        assert answer.value == 0
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x50,0x06,0x15]))
+
+
+    def test_enable_pullup_resistor(self, control, mock_socket):
+        data = bytearray([0x00,0xD1,0x00,0x00])
+        mock_socket.recv = mock.Mock()
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.enable_pullup_resistor()
+        assert answer == 0
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x51,0x00,0x15]))
+
+
+    def test_disable_the_pullup_resistor(self, control, mock_socket):
+        data = bytearray([0x00,0xD1,0x00,0x00])
+        mock_socket.recv = mock.Mock()
+        mock_socket.recv.side_effect = [0x02, 0x04, data ]
+        answer = control.disable_pullup_resistor()
+        assert answer == 0
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x04,0x00,0x51,0x03,0x15]))
+
+
+    def test_get_voltage_of_proximity_signal(self, control, mock_socket):
+        data = bytearray([0x00,0xD2,0x07,0x00,0x00])
+        mock_socket.recv = mock.Mock()
+        mock_socket.recv.side_effect = [0x02, 0x05, data ]
+        answer = control.get_voltage_of_proximity_signal()
+        assert answer == 7
+        mock_socket.send.assert_called_once_with(bytearray([0x02,0x03,0x00,0x52,0x15]))
+
+
 
 
