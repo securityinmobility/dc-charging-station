71ac80d1ff3192526737d48c3cec624ad85b799a
gisela moreira ortt <ms.giselaortt@gmail.com>
2024-05-13 23:04:00 +0200

    changed the mock_socket.recv declaration to go inside the fixture

--- TestChargebyteBoard.py
+++ TestChargebyteBoard.py
@@ -10,6 +10,7 @@ from typing import overload
 @pytest.fixture(autouse=True)
 def mock_socket(mocker):
     mock_socket = mock.Mock()
+    mock_socket.recv = mock.Mock()
     return mock_socket
 
 
@@ -41,7 +42,6 @@ class TestChargeboardByte:
     def test_should_check_length(self,control,mock_socket):
         data = bytearray([0x02,0x06,0x00,0x95,0x15,0x07,0x02])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         with pytest.raises(Exception) as info:
             control.set_ucp(2)
@@ -51,7 +51,6 @@ class TestChargeboardByte:
     def test_should_verify_service_id(self,control,mock_socket):
         data = bytearray([0x02,0x06,0x00,0x81,0x81,0x81,0x02])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         with pytest.raises(Exception) as info:
             control.send_packet(0x51, bytearray())
@@ -70,7 +69,6 @@ class TestChargeboardByte:
     def test_should_start_with_answer_code(self,control,mock_socket):
         data = bytearray([0x05,0x04,0x00,0x95,0x1])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         with pytest.raises(Exception) as info:
             control.set_ucp(2)
@@ -80,7 +78,6 @@ class TestChargeboardByte:
     def test_test_device_one(self, control, mock_socket):
         data = bytearray([0x02,0x06,0x00,0x81,0x81,0x81,0x02])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         expected_request = bytearray([0x02,0x03,0x00,0x01])
         expected_request.append(xor_calculator(expected_request))
@@ -94,7 +91,6 @@ class TestChargeboardByte:
     def test_test_device_two(self, control, mock_socket):
         data = bytearray([0x02,0x06,0x00,0x84,0x07,0x00,0x02])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         build, reset_value = control.test_device_two()
         assert build == 7
@@ -107,7 +103,6 @@ class TestChargeboardByte:
     def test_set_pwm(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x91,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         control.set_pwm(2,3)
         expected_request = bytearray([0x02,0x07,0x00,0x11,0x02,0x00,0x03,0x00])
@@ -118,7 +113,6 @@ class TestChargeboardByte:
     def test_get_pwm(self,control,mock_socket):
         data = bytearray([0x02,0x07,0x00,0x90,0x02,0x00,0x03,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         frequency, dutycicle = control.get_pwm()
         assert frequency == 2
@@ -131,7 +125,6 @@ class TestChargeboardByte:
     def test_control_pwm(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x92,0x01])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         control_code = control.control_pwm(1)
         assert control_code.value == 1
@@ -143,7 +136,6 @@ class TestChargeboardByte:
     def test_get_ucp(self,control,mock_socket):
         data = bytearray([0x02,0x07,0x00,0x94,0x06,0x00,0x02,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         positive_cp, negative_cp = control.get_ucp()
         assert positive_cp == 6
@@ -156,7 +148,6 @@ class TestChargeboardByte:
     def test_set_ucp(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x95,0x01])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         resistance = control.set_ucp(1)
         assert resistance == 1
@@ -168,7 +159,6 @@ class TestChargeboardByte:
     def test_lock_and_unlock_cable_one(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x97,0x01])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.lock_unlock_cable_one(2)
         assert answer.value == 1
@@ -180,7 +170,6 @@ class TestChargeboardByte:
     def test_lock_and_unlock_cable_two(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x98,0x01])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.lock_unlock_cable_two(2)
         assert answer.value == 1
@@ -192,7 +181,6 @@ class TestChargeboardByte:
     def test_get_motor_fault_pin(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0x9A,0x81])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.get_motor_fault_pin()
         assert answer.value == 1
@@ -204,7 +192,6 @@ class TestChargeboardByte:
     def test_set_cyclic_process_data(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0xA0,0x01])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.set_cyclic_process_data(4)
         assert answer.value == 1
@@ -220,7 +207,6 @@ class TestChargeboardByte:
     def test_push_button_simple_connect(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0xB1,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.push_button_simple_connect(1)
         assert answer.value == 0
@@ -232,7 +218,6 @@ class TestChargeboardByte:
     def test_reset(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0xB3,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         control.reset()
         expected_request = bytearray([0x02,0x03,0x00,0x33])
@@ -243,7 +228,6 @@ class TestChargeboardByte:
     def test_activate_proximity_pilot_resistor_(self,control,mock_socket):
         data = bytearray([0x02,0x04,0x00,0xD0,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.activate_proximity_pilot_resistor(0x06)
         assert answer.value == 0
@@ -255,7 +239,6 @@ class TestChargeboardByte:
     def test_enable_pullup_resistor(self, control, mock_socket):
         data = bytearray([0x02,0x04,0x00,0xD1,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.enable_pullup_resistor()
         expected_request = bytearray([0x02,0x04,0x00,0x51,0x03])
@@ -266,7 +249,6 @@ class TestChargeboardByte:
     def test_disable_pullup_resistor(self, control, mock_socket):
         data = bytearray([0x02,0x04,0x00,0xD1,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.disable_pullup_resistor()
         expected_request = bytearray([0x02,0x04,0x00,0x51,0x00])
@@ -277,7 +259,6 @@ class TestChargeboardByte:
     def test_get_voltage_of_proximity_signal(self, control, mock_socket):
         data = bytearray([0x02,0x05,0x00,0xD2,0x07,0x00])
         data.append(xor_calculator(data))
-        mock_socket.recv = mock.Mock()
         mock_socket.recv.side_effect = [data[0], data[1], data[2:] ]
         answer = control.get_voltage_of_proximity_signal()
         assert answer == 7
