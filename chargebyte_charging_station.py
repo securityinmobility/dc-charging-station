d44929fd19d6750bc216b4ed8ae83b0bb850c63e
giselaortt <ms.giselaortt@gmail.com>
2024-09-24 11:31:54 +0200

    started the high level.

--- chargebyte_charging_station.py
+++ chargebyte_charging_station.py
@@ -2,6 +2,9 @@ import chargebyte_board
 
 
 class CharbyteChargingStation:
+    def __init__(self, host, port):
+        self.cbb = chargebyte_board.ChargebyteBoard(host, port)
+
     def isVehicleDetected(self) -> bool:
         pass
 
@@ -9,5 +12,10 @@ class CharbyteChargingStation:
         pass
 
     def setPWM(self, dutycycle: float):
-        pass
+        self.enablePWM()
+        self.cbb.set_pwm(1000, dutycicle)
+
+    def enablePWM(self)->None:
+        cbb.control_pwm(chargebyte_board.ControlCode(1))
+
 
