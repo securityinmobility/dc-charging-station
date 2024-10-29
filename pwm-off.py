f256f04a2edbcc8c7cfcf7bbda03e9aac0999ef9
Jakob Löw <jakob@m4gnus.de>
2024-10-29 09:32:42 +0100

    :sparkles: add two new scripts for enabling/disabling EVSE mode (PWM & PP resistor)

---
+++ pwm-off.py
@@ -0,0 +1,17 @@
+import time
+from chargebyte.chargebyte_board import ChargebyteBoard, ControlCode, ResistorCode
+
+# run the following command on the chargebyte board to make this work:
+# socat tcp-l:2020,reuseaddr,fork,crlf file:/dev/ttyAPP2,echo=0,b57600,raw
+
+board = ChargebyteBoard("192.168.188.250", 2020)
+
+board.control_pwm(ControlCode.DISABLE.value)
+time.sleep(5)
+
+board.disable_pullup_resistor()
+board.activate_proximity_pilot_resistor(ResistorCode.OFF.value)
+time.sleep(1)
+
+print(board.get_pwm())
+print(board.get_cp())
