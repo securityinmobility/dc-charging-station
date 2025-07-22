0639b18245ffbb3eead14a76d5c4acddd9ed8394
Ferdinand Schleeh <ferdinand.Schleeh@outlook.com>
2025-07-22 15:00:19 +0000

    :art: Improved SW-Quality, address review issues

--- iso15118impls/slac_session.py
+++ iso15118impls/slac_session.py
@@ -1,3 +1,4 @@
+from binascii import unhexlify
 from pyslac.utils import is_distro_linux
 
 if not is_distro_linux():
@@ -10,6 +11,7 @@ import logging
 import types
 import os
 from typing import List, Optional
+from binascii import unhexlify
 
 from pyslac.environment import Config
 from pyslac.session import SlacEvseSession, SlacSessionController
@@ -19,6 +21,32 @@ from base_classes import ChargingStation
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__file__)
+
+class SpecificSlacEvseSession(SlacEvseSession):
+    """
+    A subclass of SlacEvseSession that overrides the evse_set_key method
+    to work with the ChargebyteChargingStation.
+    """
+    def __init__(self, evse_id: str, network_interface: str, slac_config: Config, NMK: str, NID: str):
+        super().__init__(evse_id, network_interface, slac_config)
+        self.NMK = NMK
+        self.NID = NID
+
+    async def evse_set_key(self):
+        """
+        Set the EVSE key for the session.
+        This function is adapted to work with the ChargebyteChargingStation.
+        """
+        logger.info("CM_SET_KEY: Started...")
+        # Set NMK and NID with the environment variables NMK and NID 
+        nmk = unhexlify(self.NMK)
+        nid = unhexlify(self.NID)
+        logger.debug("New NMK: %s", self.NMK)
+        logger.debug("New NID: %s", self.NID)
+
+        self.nmk = nmk
+        self.nid = nid
+        logger.info("CM_SET_KEY: Finished!")
 
 
 class SlacHandler(SlacSessionController):
@@ -46,22 +74,19 @@ class SlacHandler(SlacSessionController):
         self.low_level_abstraction.set_pwm_duty_cycle(5.0)
         logger.info(f"Enable PWM and set 5% duty cycle for evse {evse_id}")
 
-    async def start(self, cs_config: dict):
-        if cs_config["number_of_evses"] < 1 or (
-            len(cs_config["parameters"]) != cs_config["number_of_evses"]
-        ):
-            raise AttributeError("Number of evses provided is invalid.")
+    async def start(self, network_interface: str, evse_id: str):
+        if network_interface is None:
+            raise ValueError("Network interface must be provided")
 
-        evse_params: dict = cs_config["parameters"][0]
-        evse_id: str = evse_params["evse_id"]
-        network_interface: str = evse_params["network_interface"]
+        if evse_id is None:
+            raise ValueError("EVSE ID must be provided")
+
         try:
-            slac_session = SlacEvseSession(evse_id, network_interface, self.slac_config)
-
             if os.environ.get("NMK") and os.environ.get("NID"):
-                slac_session.evse_set_key = types.MethodType(custom_evse_set_key, slac_session)
+                slac_session = SpecificSlacEvseSession(evse_id, network_interface, self.slac_config, os.environ.get("NMK"), os.environ.get("NID"))
                 await slac_session.evse_set_key()
             else:
+                slac_session = SlacEvseSession(evse_id, network_interface, self.slac_config)
                 await slac_session.evse_set_key()
 
             self.running_sessions.append(slac_session)
@@ -86,67 +111,6 @@ class SlacHandler(SlacSessionController):
             await asyncio.sleep(0.2)  # Polling interval
 
 
-# adapted evse_set_key function for the specific chargebyte charging station
-async def custom_evse_set_key(self) -> bytes:
-    """
-    Set the EVSE key for the session.
-    This function is adapted to work with the ChargebyteChargingStation.
-    """
-    from binascii import unhexlify
-    from pyslac.layer_2_headers import EthernetHeader, HomePlugHeader
-    from pyslac.messages import SetKeyReq, SetKeyCnf
-    from pyslac.enums import FramesSizes, Timers, MMTYPE_REQ, CM_SET_KEY, SLAC_SETTLE_TIME
-
-    logger.info("CM_SET_KEY: Started...")
-    # Set NMK and NID with the environment variables NMK and NID 
-    nmk = unhexlify(os.environ.get("NMK"))
-    nid = unhexlify(os.environ.get("NID"))
-    logger.debug("New NMK: %s", os.environ.get("NMK"))
-    logger.debug("New NID: %s", os.environ.get("NID"))
-    ethernet_header = EthernetHeader(
-        dst_mac=self.evse_plc_mac, src_mac=self.evse_mac
-    )
-    homeplug_header = HomePlugHeader(CM_SET_KEY | MMTYPE_REQ)
-    key_req_payload = SetKeyReq(nid=nid, new_key=nmk)
-
-    frame_to_send = (
-        ethernet_header.pack_big()
-        + homeplug_header.pack_big()
-        + key_req_payload.pack_big()
-    )
-
-    # TODO: Change this to just open a socket once for every SlacSession
-    # and not every time we call send or send_recv_eth
-    # Also think about including the send, rcv method as inner methods of
-    # SetKeyReq. Maybe even create a class SetKey that handles both the
-    # Send and the CNF of the message
-    try:
-        await self.send_frame(frame_to_send)
-        data_rcvd = await self.rcv_frame(
-            rcv_frame_size=FramesSizes.CM_SET_KEY_CNF,
-            timeout=Timers.SLAC_INIT_TIMEOUT,
-        )
-    except asyncio.TimeoutError as e:
-        raise TimeoutError("SetKey Timeout raised") from e
-    try:
-        SetKeyCnf.from_bytes(data_rcvd)
-        self.nmk = nmk
-        self.nid = nid
-    except ValueError as e:
-        logger.error(e)
-        if self.nmk and self.nid:
-            logger.debug(
-                "SetKeyReq has failed, old NMK: %s and NID: %s apply",
-                self.nmk,
-                self.nid,
-            )
-        else:
-            raise ValueError("SetKeyCnf data parsing into the class failed") from e
-    logger.debug("Registering NMK and NID into the PLC node...")
-    await asyncio.sleep(SLAC_SETTLE_TIME)
-    logger.info("CM_SET_KEY: Finished!")
-    return data_rcvd
-
 def get_slac_handler(low_level_abstraction: ChargingStation, env_path: Optional[str] = None) -> SlacHandler:
     """
     Factory function to create a SlacHandler instance with the given low-level abstraction.
@@ -156,23 +120,5 @@ def get_slac_handler(low_level_abstraction: ChargingStation, env_path: Optional[
 
     return SlacHandler(slac_config, low_level_abstraction)
 
-def get_cs_config(network_interface) -> dict:
-    """
-    Factory function to get the charging station configuration.
-    """
-    CONFIG_PATH = Path(__file__).parent / "cs_configuration.json"
-
-    try:
-        with open(CONFIG_PATH, "r") as f:
-            cs_config = json.load(f)
-            if cs_config["number_of_evses"] == 1:
-                cs_config["parameters"][0]["network_interface"] = network_interface
-    except json.JSONDecodeError as e:
-        logger.error(f"Error decoding JSON from {CONFIG_PATH}: {e}")
-        return {}
-    except FileNotFoundError:
-        logger.error("cs_configuration.json not found. SLAC cannot be initialized.")
-        return {}
-    return cs_config
 
 
